3ad7c1cc2e309a6a16bdf3cc5352748a7206ef77
Ievgen <ievgen.bezuglyi@gmail.com>
2015-06-13 12:12:01 +0200

    knockout

--- vms/main.js
+++ vms/main.js
@@ -1,21 +1,4 @@
-App = {};
-
-(function(app){
-  var viewModel = {};
-  viewModel.name = ko.observable();
-  App.main = viewModel;
-  var messagesRef = new Firebase('https://glaring-heat-5949.firebaseio.com/');
-
-  messagesRef.set({
-    places:[
-
-      {
-        location:"",
-        img:"",
-        time:""
-      }
-    ]
-});
-
-  ko.applyBindings(app.main);
-}(App));
+var Place = function(){
+  this.name = ko.observable();
+  this.locations= ko.observable();
+}
