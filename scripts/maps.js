5af6695ab70b0c25b20faa280a6054801bfacc29
Mariusz Kowalski <mk@devpad.me>
2015-06-13 17:04:06 +0200

    Maps cursor

--- scripts/maps.js
+++ scripts/maps.js
@@ -4,6 +4,7 @@ function initialize() {
     navigator.geolocation.getCurrentPosition(function(pos) {
       var lat = pos.coords.latitude;
       var lon = pos.coords.longitude;
+      console.log("current pos: " + lat + "x" + lon)
       var mapOptions = {
         zoom: 8,
         center: new google.maps.LatLng(lat, lon)
@@ -12,6 +13,10 @@ function initialize() {
               document.getElementById('map-canvas'),
               mapOptions
             );
+
+      google.maps.event.addListener(map, 'click', function(event) {
+        set_cursor(event.latLng);
+      });
     });
   }
 }
@@ -31,4 +36,29 @@ function add_marker(place){
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.open(map, marker);
   });
+}
+
+var cursor;
+var cursor_callback = function(a, f){
+  console.log("Cursor at: " + a + "x" + f)
+};
+
+function set_cursor(location) {
+  // prepare content info
+  var content = "<div>to jest info " + Math.random() + "</div>";
+
+  if((typeof cursor) === 'undefined'){
+    // add cursor
+    cursor = new google.maps.Marker({
+      position: location,
+      map: map
+    });
+  }else{
+    // move cursor
+    cursor.setPosition(location);
+  }
+
+  if((typeof cursor_callback) !== 'undefined'){
+    cursor_callback(location.A, location.F);
+  }
 }
