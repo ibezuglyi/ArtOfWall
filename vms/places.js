ca30504eff67c167b5ef0c62ac410b35b022a7e8
Mariusz Kowalski <mk@devpad.me>
2015-06-13 12:58:42 +0200

    Działa wyświetlanie

--- vms/places.js
+++ vms/places.js
@@ -2,6 +2,7 @@ var Places = function(db) {
   var _self = this;
   this.places = ko.observableArray();
   db.child("places").on("value", function(snapshot) {
+    console.log(snapshot.val());
     var collection = _.map(snapshot.val(), function(place_data){
       return new Place(place_data);
     });
