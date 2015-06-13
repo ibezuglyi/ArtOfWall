a394eb230bb0d260a2c09b30c4727c98aa886bbd
Ievgen <ievgen.bezuglyi@gmail.com>
2015-06-13 17:11:21 +0200

    merge

--- scripts/place.js
+++ scripts/place.js
@@ -2,15 +2,8 @@ var Place = function(data){
   this.lat = ko.observable(data.lat);
   this.lon = ko.observable(data.lon);
   this.img = ko.observable(data.img);
-<<<<<<< HEAD
-  add_marker(this);
-=======
   this.name = ko.observable(data.name);
   this.description = ko.observable(data.description);
+  add_marker(this);
 
-
-  if(typeof map !== "undefined"){
-    map.add_marker(this);
-  }
->>>>>>> 4f6277e0146ec0a2be93b32afc928fb2985efeef
 }
