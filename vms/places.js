a65e23f0118c8cfbfd7347f46afbb820d41395cf
Ievgen <ievgen.bezuglyi@gmail.com>
2015-06-13 13:23:51 +0200

    db

--- vms/places.js
+++ vms/places.js
@@ -1,8 +1,23 @@
 var Places = function(db) {
+  this.db = db;
   var _self = this;
+  this.newLocation = ko.observable();
+  this.newImg = ko.observable();
   this.places = ko.observableArray();
+
+  this.onAddNewPlace = function(){
+    db.push({"places": {
+      location: _self.newLocation(),
+      img:_self.newImg()
+    }});
+
+    _self.newImg("");
+    _self.newLocation("");
+  }
+
   db.child("places").on("value", function(snapshot) {
     var collection = _.map(snapshot.val(), function(place_data){
+      console.log(place_data);
       return new Place(place_data);
     });
     _self.places(collection);
